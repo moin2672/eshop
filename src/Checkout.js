866bbfa987ecc81e9b383b80cec209945840e851
moin2672 <moin2672@gmail.com>
2022-12-27 18:00:11 +0530

    5. eshop react routing

---
+++ src/Checkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Checkout.css'
+
+function Checkout() {
+  return (
+    <div>
+        <h1>Hello Guest.. you're in Checkout page</h1>
+    </div>
+  )
+}
+
+export default Checkout
