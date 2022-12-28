7b8c952496cc0b4402705b23304107f257afa8db
moin2672 <moin2672@gmail.com>
2022-12-28 17:00:15 +0530

    8. add to basket

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,26 +1,25 @@
 import React from "react";
 import "./CheckoutProduct.css";
 
-function CheckoutProduct() {
+function CheckoutProduct({id, title, image, price, rating}) {
   return (
     <div>
       <div className="checkoutProduct">
         <img
-          src="https://images-na.ssl-images-amazon.com/images/I/71mEsHyzSCL._SL1000_.jpg"
+          src={image}
           alt=""
           className="checkoutProduct__image"
         />
         <div className="checkoutProduct__info">
           <p className="checkoutProduct__title">
-            Bennett Mystic 15.6 inch Laptop Shoulder Messenger Sling Office Bag,
-            Water Repellent Fabric for Men and Women (Blue)
+           {title}
           </p>
           <p className="checkoutProduct__price">
             <small>$</small>
-            <strong>45.95</strong>
+            <strong>{price}</strong>
           </p>
           <div className="checkoutProduct__rating">
-            {Array(3)
+            {Array(rating)
               .fill()
               .map((_, i) => (
                 <p>⭐</p>
